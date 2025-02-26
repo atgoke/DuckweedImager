6fefcf011cbd06525d7b8bd6cb9bdb070f0e1711
Alex Goke <alexgoke@gmail.com>
2025-02-26 08:50:34 -0800

    Update DuckweedImager.py

---
+++ DuckweedImager.py
@@ -0,0 +1,5 @@
+# Code for the Duckweed Imaging system - a GUI to capture and name image files with Raspberry Pi Camera Module 3 and the Raspberry Pi 4b. 
+
+# Import libraries and functions
+from libcamera import controls
+from picamera2 import Picamera2
